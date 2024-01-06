d4cb4dbcb46192c44db1f307ef4fdb3df71b36a5
Rareshp <155653633+Rareshp@users.noreply.github.com>
2024-01-06 19:55:39 +0200

    initial commit

---
+++ python_unique_char_sequence.py
@@ -0,0 +1,20 @@
+# https://adventofcode.com/2022/day/6
+import sys, subprocess
+
+# catch if user didnt give argument
+if len(sys.argv) > 1:
+    buffer = sys.argv[1]
+else:
+    print(f"Usage: {sys.argv[0]} 'sequence'")
+    quit()
+
+i = 0
+# NOTE: as the problem asks to find 4 chars, window=4
+window = 4
+while i < len(buffer):
+    current = buffer[i:i+window]
+    current_set = set(current)
+    if len(current_set) == window:
+        print(f"Found solution: '{current}' starting at index {i}. Answer {i+4}")
+        break
+    i += 1
